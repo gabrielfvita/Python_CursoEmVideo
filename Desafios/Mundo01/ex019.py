45e11411fa36f6b88eccbb00d098be509d9c52ef
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:26 -0300

    Aula 8: Sorteando um item na lista

--- Desafios/Mundo01/ex019.py
+++ Desafios/Mundo01/ex019.py
@@ -3,6 +3,7 @@ p = str(input('Digite o primeiro aluno'))
 s = str(input('Digite o segundo aluno'))
 t = str(input('Digite o terceiro aluno'))
 q = str(input('Digite o quarto aluno'))
+
 lista = [p, s, t, q]
 escolhido = choice(lista)
 print(escolhido)
