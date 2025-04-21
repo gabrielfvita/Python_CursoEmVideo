b4313ef3f992516484b2aa84b97014a281b501ca
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 20:05:26 -0300

    Aula 12: Analisando triângulos v2.0

--- Desafios/Mundo02/ex042.py
+++ Desafios/Mundo02/ex042.py
@@ -1,6 +1,6 @@
-l1 = float(input('Digite a medida do primeiro lado:'))
-l2 = float(input('Digite a medida do segundo lado:'))
-l3 = float(input('Digite a medida do terceiro lado:'))
+l1 = float(input('Digite a medida do primeiro lado: '))
+l2 = float(input('Digite a medida do segundo lado: '))
+l3 = float(input('Digite a medida do terceiro lado: '))
 
 if l1 == l2 == l3:
     tipo = 'Equilátero'
