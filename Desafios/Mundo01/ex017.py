f85a1bc6e8fcc98513887cd82898f317c540715d
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:26 -0300

    Aula 8: Catetos e hipotenusa

--- Desafios/Mundo01/ex017.py
+++ Desafios/Mundo01/ex017.py
@@ -2,4 +2,5 @@ from math import hypot
 catop = float(input('Qual a medida do cateto oposto?'))
 catadj = float(input('Qual a medida do cateto adjacente?'))
 hip = hypot(catop,catadj)
+
 print('A medida da hipotenusa é de {:.2f}'.format(hip))
