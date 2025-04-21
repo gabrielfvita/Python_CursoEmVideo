166a314a98a4d83d5ed3094b66faf019f1405876
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:52:13 -0300

    Aula 06: Dissecando uma variável

--- Desafios/Mundo01/ex004.py
+++ Desafios/Mundo01/ex004.py
@@ -1,5 +1,7 @@
 teclado = input('Digite algo para obter informaçoes: ')
+
 tipo = type(teclado)
+
 print('O tipo primitivo da palavra digitada é: {}'.format(tipo))
 print('Só tem espaços?', teclado.isspace())
 print('É um número?', teclado.isnumeric())
