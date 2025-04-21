1d02b96ad4d744e9bdb458e88514ace0771e6150
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:52:13 -0300

    Aula 07: Calculando descontos

--- Desafios/Mundo01/ex012.py
+++ Desafios/Mundo01/ex012.py
@@ -1,4 +1,5 @@
 produto = int(input('Digite o preço do produto: '))
 descontar = produto*0.05
 desconto = produto-descontar
+
 print('O novo valor do produto com desconto é de R${:.2f}:'.format(desconto))
