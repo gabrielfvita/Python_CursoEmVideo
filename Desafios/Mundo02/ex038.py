e51496f9e64cf77c964cfb3abc14007617d358ca
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 20:05:26 -0300

    Aula 12: Comparando números

--- Desafios/Mundo02/ex038.py
+++ Desafios/Mundo02/ex038.py
@@ -1,5 +1,5 @@
-num1 = int(input('Digite o primeiro número:'))
-num2 = int(input('Digite o segundo número:'))
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
 
 if num1 > num2:
     print('O primeiro valor é o maior')
