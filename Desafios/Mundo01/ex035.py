c04debe2ae26786d191d0d4ccb2c0e3e6d271dd0
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:39 -0300

    Aula 10: Analisando triângulo v.1.0

--- Desafios/Mundo01/ex035.py
+++ Desafios/Mundo01/ex035.py
@@ -1,6 +1,6 @@
-l1 = float(input('Digite a medida do primeiro lado:'))
-l2 = float(input('Digite a medida do segundo lado:'))
-l3 = float(input('Digite a medida do terceiro lado:'))
+l1 = float(input('Digite a medida do primeiro lado: '))
+l2 = float(input('Digite a medida do segundo lado: '))
+l3 = float(input('Digite a medida do terceiro lado: '))
 if (l1+l2) > l3 and (l1+l3) > l2 and (l2+l3) > l1:
     print('Você pode formar um triangulo com essas medidas!')
 else:
