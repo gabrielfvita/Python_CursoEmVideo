a316e238f90119117cab959ea45e9ed8fdd5ff66
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:39 -0300

    Aula 10: Jogo da adivinhação v.1.0

--- Desafios/Mundo01/ex028.py
+++ Desafios/Mundo01/ex028.py
@@ -1,7 +1,7 @@
 from random import randint
 numero = randint(0,5)
 
-chute = int(input('Tente acertar o número entre 0 e 5:'))
+chute = int(input('Tente acertar o número entre 0 e 5: '))
 if chute == numero:
     print('Você acertou, parabens!')
 else:
