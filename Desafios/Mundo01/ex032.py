3d56edfc0bd01f0a8d5f7ac1152f19afcff36052
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:39 -0300

    Aula 10: Ano bissexto

--- Desafios/Mundo01/ex032.py
+++ Desafios/Mundo01/ex032.py
@@ -1,5 +1,5 @@
 from datetime import date
-ano = int(input('Digite um ano para analisar se é Bisexto. Digite 0 para o ano atual.'))
+ano = int(input('Digite um ano para analisar se é Bisexto. Digite 0 para o ano atual. '))
 if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
