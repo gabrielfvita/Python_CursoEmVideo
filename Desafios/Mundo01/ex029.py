55e9ab3540bfdde89b815192ea1a08c60736a4df
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 19:59:39 -0300

    Aula 10: Radar eletrônico

--- Desafios/Mundo01/ex029.py
+++ Desafios/Mundo01/ex029.py
@@ -1,4 +1,4 @@
-vel = float(input('Digite a velocidade do seu carro:'))
+vel = float(input('Digite a velocidade do seu carro: '))
 if vel > 80.0:
     print('Você está acima do limite e foi multado! O valor da multa é de: R${:.2f}'.format((vel-80)*7.0))
 else:
