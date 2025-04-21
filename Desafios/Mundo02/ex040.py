83af7365157267b786e109df58c4a3921bd0f64e
Gabriel Vita <gabrielfvita@bol.com.br>
2025-04-21 20:05:26 -0300

    Aula 12: Aquele clássico da média

--- Desafios/Mundo02/ex040.py
+++ Desafios/Mundo02/ex040.py
@@ -1,5 +1,5 @@
-nota1 = float(input('Informe a primeira nota:'))
-nota2 = float(input('Informe a segunda nota:'))
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
 media = (nota1 + nota2) / 2
 
 print(f'A média do aluno é de {media:.2f}')
