f175966ef75e533709e30b9443d86f149e5ea0bf
Gabriel Vita <gabrielfvita@bol.com.br>
2025-05-13 16:34:00 -0300

    Ex066 - Aula 15: Vários números com flag

---
+++ Desafios/Mundo02/ex066.py
@@ -0,0 +1,10 @@
+soma = 0
+contador = 0
+while True:
+    n = int(input('Digite um numero inteiro: (999 para parar) '))
+    if n == 999:
+        break
+    else:
+        soma += n
+        contador += 1
+print(f'A soma dos {contador} números é igual a: {soma}')
