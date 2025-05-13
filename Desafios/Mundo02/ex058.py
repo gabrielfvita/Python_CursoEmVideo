b2ad68e51461f9e213612774bba0a1e75f375d42
Gabriel Vita <gabrielfvita@bol.com.br>
2025-05-13 16:33:48 -0300

    Ex058 - Aula 14: Jogo da Adivinhação v.2.0

---
+++ Desafios/Mundo02/ex058.py
@@ -0,0 +1,8 @@
+from random import randint
+numero = randint(0,10)
+contador = 1
+chute = int(input('Tente acertar o número entre 0 e 10: '))
+while chute != numero:
+    chute = int(input('Não foi dessa vez... digite outro numero: '))
+    contador += 1
+print(f'Parabéns! Você acertou com {contador} tentativa(s). \nO número era: {numero}')
