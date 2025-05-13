72657731cb5b56b5ac7605d50a30dc9391ea1b8a
Gabriel Vita <gabrielfvita@bol.com.br>
2025-05-13 16:34:00 -0300

    Ex060 - Aula 14: Cálculo do Fatorial

---
+++ Desafios/Mundo02/ex060.py
@@ -0,0 +1,10 @@
+num = int(input('Digite um número: '))
+atual = num
+fatorial = f'{atual}'
+resultado = 1
+
+while atual != 1:
+    resultado = resultado * atual
+    atual -= 1
+    fatorial += f' x {atual}'
+print(f'{num}! = {fatorial} = {resultado}')
