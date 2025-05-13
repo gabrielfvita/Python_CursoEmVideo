0fd72d23a1976f65e48b6e4e9ba1ea4f34b8b88d
Gabriel Vita <gabrielfvita@bol.com.br>
2025-05-13 16:34:00 -0300

    Ex071 - Aula 15: Simulador de Caixa Eletrônico

---
+++ Desafios/Mundo02/ex071.py
@@ -0,0 +1,21 @@
+print('-------- Banco --------')
+
+valor = int(input('Informe um valor para sacar: R$'))
+
+notas50 = valor // 50
+resto = valor % 50
+notas20 = resto // 20
+resto = resto % 20
+notas10 = resto // 10
+notas1 = resto % 10
+
+if notas50 > 0:
+    print(f'Total de {notas50} cédulas de R$50')
+if notas20 > 0:
+    print(f'Total de {notas20} cédulas de R$20')
+if notas10 > 0:
+    print(f'Total de {notas10} cédulas de R$10')
+if notas1 > 0:
+    print(f'Total de {notas1} cédulas de R$1')
+
+print('-----------------------')
