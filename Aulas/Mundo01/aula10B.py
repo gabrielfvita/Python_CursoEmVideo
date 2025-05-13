d13769b7ae5458d7676f753d638c9cd3ee811ca1
Gabriel Vita <gabrielfvita@bol.com.br>
2025-05-13 16:43:11 -0300

    Aula 10 - Condições em Python (if, elif, else)

--- Aulas/Mundo01/aula10B.py
+++ Aulas/Mundo01/aula10B.py
@@ -1,5 +1,5 @@
-n1 = float(input('Digite a primeira nota:'))
-n2 = float(input('Digite a segunda nota:'))
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2) / 2
 print('A sua média foi: {:.1f}'.format(m))
 print('Parabéns, sua média foi boa!' if m >= 6.0 else 'Sua média está baixa, estude mais!')
